240f127ac4dc7d970273c91aec9ec4632fab8d75
Sai Vishal <saivishal2001@gmail.com>
2016-06-16 07:38:57 +0530

    Create install_req.sh

---
+++ install_req.sh
@@ -0,0 +1,1 @@
+apt-get install bison build-essential curl flex git gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop maven openjdk-7-jdk openjdk-7-jre pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev bc g++-multilib gcc-multilib lib32ncurses5-dev lib32readline6-dev lib32z1-dev
