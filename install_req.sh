659cbfcf028c3e095951e93eaa925f4a6587e8bf
Sai Vishal <saivishal2001@gmail.com>
2016-06-16 07:43:57 +0530

    Update install_req.sh

--- install_req.sh
+++ install_req.sh
@@ -1,1 +1,4 @@
+# Copyright Sai Vishal 2016
+# You are free to make changes and distribute this shell script, only if the above line is present
+#
 apt-get install bison build-essential curl flex git gnupg gperf libesd0-dev liblz4-tool libncurses5-dev libsdl1.2-dev libwxgtk2.8-dev libxml2 libxml2-utils lzop maven openjdk-7-jdk openjdk-7-jre pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev bc g++-multilib gcc-multilib lib32ncurses5-dev lib32readline6-dev lib32z1-dev
